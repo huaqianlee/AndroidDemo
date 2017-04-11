aca8ae3b47d2123fe88e64140cd2dc6667bbfca2
huaqianlee <huaqianlee@gmail.com>
2017-04-11 16:46:16 +0800

    update meterial test

---
+++ FirstCode/chapter12/app/src/main/java/com/example/lee/materialtest/Fruit.java
@@ -0,0 +1,23 @@
+package com.example.lee.materialtest;
+
+/**
+ * Created by lee on 17-4-11.
+ */
+
+public class Fruit {
+    private String name;
+    private int imageId;
+
+    public Fruit(String name, int imageId) {
+        this.name = name;
+        this.imageId = imageId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getImageId() {
+        return imageId;
+    }
+}
