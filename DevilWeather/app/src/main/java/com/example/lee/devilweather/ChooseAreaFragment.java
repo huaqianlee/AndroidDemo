3203e759e3b464daaf49146f7de0833c395f5342
huaqianlee <huaqianlee@gmail.com>
2017-06-16 15:09:21 +0800

    update

---
+++ DevilWeather/app/src/main/java/com/example/lee/devilweather/ChooseAreaFragment.java
@@ -0,0 +1,229 @@
+package com.example.lee.devilweather;
+
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.lee.devilweather.db.City;
+import com.example.lee.devilweather.db.County;
+import com.example.lee.devilweather.db.Province;
+import com.example.lee.devilweather.util.OkHttpUtil;
+import com.example.lee.devilweather.util.Utility;
+
+import org.litepal.crud.DataSupport;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
+
+/**
+ * Created by lee on 16/06/17.
+ */
+
+public class ChooseAreaFragment extends Fragment {
+    public static final int LEVEL_PROVINCE = 0;
+    public static final int LEVEL_CITY = 1;
+    public static final int LEVEL_COUNTY =2;
+    private ProgressDialog progressDialog;
+    private TextView titleText;
+    private Button backButton;
+    private ListView listView;
+    private ArrayAdapter<String> adapter;
+    private List<String> dataList = new ArrayList<>();
+    /*province*/
+    private List<Province> provinceList;
+    /*city*/
+    private List<City> cityList;
+    /*county*/
+    private List<County> countyList;
+    /*selected province*/
+    private Province selectedProvince;
+    /*selected city*/
+    private City selectedCity;
+    /*current level*/
+    private int currentLevel;
+
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.choose_area, container, false);
+        titleText = (TextView) view.findViewById(R.id.title_text);
+        backButton = (Button) view.findViewById(R.id.back_button);
+        listView = (ListView) view.findViewById(R.id.list_view);
+        adapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_list_item_1, dataList);
+        listView.setAdapter(adapter);
+        return view;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        queryProvinces(); // load provinces data firstly
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                if (currentLevel == LEVEL_PROVINCE) {
+                    selectedProvince = provinceList.get(position);
+                    queryCities();
+                } else if (currentLevel == LEVEL_CITY) {
+                    selectedCity = cityList.get(position);
+                    queryCounties();
+                }
+            }
+        });
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (currentLevel == LEVEL_COUNTY) {
+                    queryCities();
+                } else if (currentLevel == LEVEL_CITY) {
+                    queryProvinces();
+                }
+            }
+        });
+
+    }
+
+    private void queryProvinces() {
+        titleText.setText(R.string.china);
+        backButton.setVisibility(View.GONE);
+        provinceList = DataSupport.findAll(Province.class);
+        if (provinceList.size() > 0) {
+            dataList.clear();
+            for (Province province: provinceList){
+                dataList.add(province.getName());
+            }
+            adapter.notifyDataSetChanged();
+            listView.setSelection(0);
+
+        } else {
+            String address = "http://guolin.tech/api/china";
+            queryFromServer(address,"province");
+        }
+
+    }
+
+    private void queryCities () {
+        titleText.setText(selectedProvince.getName());
+        backButton.setVisibility(View.VISIBLE);
+        cityList = DataSupport.where("provinceid=?", String.valueOf(selectedProvince.getId())).find(City.class);
+        if (cityList.size() > 0) {
+            dataList.clear();
+            for (City city: cityList) {
+                dataList.add(city.getName());
+            }
+            adapter.notifyDataSetChanged();
+            listView.setSelection(0);
+            currentLevel = LEVEL_CITY;
+        } else {
+            int code = selectedProvince.getCode();
+            String address = "http://guolin.tech/api/china/" + code;
+            queryFromServer(address,"city");
+        }
+    }
+
+
+    private void queryCounties () {
+        titleText.setText(selectedCity.getName());
+        backButton.setVisibility(View.VISIBLE);
+        countyList = DataSupport.where("cityid=?", String.valueOf(selectedCity.getId())).find(County.class);
+        if (countyList.size() > 0) {
+            dataList.clear();
+            for (County county : countyList) {
+                dataList.add(county.getName());
+            }
+            adapter.notifyDataSetChanged();
+            listView.setSelection(0);
+
+        } else {
+            int provinceCode = selectedProvince.getCode();
+            int cityCode = selectedCity.getCode();
+            String address = "http://guolin.tech/api/china/" + provinceCode + "/" + cityCode;
+            queryFromServer(address, "county");
+        }
+    }
+
+    private void queryFromServer(String address, final String type) {
+        showProgressDialog();
+        OkHttpUtil.sendOkHttpRequest(address, new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                getActivity().runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        closeProgressDialog();
+                        Toast.makeText(getContext(), getString(R.string.okhttp_info), Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                String responseText = response.body().string();
+                boolean result = false;
+                if ("province".equals(type)) {
+                    result = Utility.handleProvinceData(responseText);
+                } else if ("city".equals(type)) {
+                    result = Utility.handleCityData(responseText, selectedProvince.getId());
+                } else if ("County".equals(type)) {
+                    result = Utility.handleCountyData(responseText, selectedCity.getId());
+                }
+
+                if (result) {
+                    getActivity().runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            closeProgressDialog();
+                            if ("provice".equals(type)) {
+                                queryProvinces();
+                            } else if ("city".equals(type)) {
+                                queryCities();
+                            } else if ("county".equals(type)) {
+                                queryCounties();
+                            }
+                        }
+                    });
+                }
+
+            }
+        });
+    }
+
+    private void closeProgressDialog() {
+
+        if (progressDialog != null) {
+            progressDialog.dismiss();
+        }
+    }
+
+    private void showProgressDialog() {
+        if (progressDialog == null ) {
+            progressDialog = new ProgressDialog(getActivity());
+            progressDialog.setMessage(getString(R.string.loading));
+            progressDialog.setCanceledOnTouchOutside(false);
+
+        }
+        progressDialog.show();
+
+    }
+
+}
--- DevilWeather/app/src/main/java/com/example/lee/devilweather/util/Utility.java
+++ DevilWeather/app/src/main/java/com/example/lee/devilweather/util/Utility.java
@@ -1,8 +1,16 @@
 package com.example.lee.devilweather.util;
 
+import android.text.TextUtils;
+
+import com.example.lee.devilweather.db.City;
+import com.example.lee.devilweather.db.County;
 import com.example.lee.devilweather.db.Province;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.List;
 
@@ -11,16 +19,82 @@ import java.util.List;
  */
 
 public class Utility {
+    /*Parse and handle province data*/
     public static boolean handleProvinceData (String jsonData) {
-        Gson gson = new Gson();
-        List<Province> provinces = gson.fromJson(jsonData, new TypeToken<Province>(){}.getType());
-        for (Province province :
-                provinces) {
-            if (province == null) {
-                return false;
+/*        if (!TextUtils.isEmpty(jsonData)) {
+            Gson gson = new Gson();
+            List<Province> provinces = gson.fromJson(jsonData, new TypeToken<Province>() {
+            }.getType());
+            for (Province province :
+                    provinces) {
+                if (province == null) {
+                    return false;
+                }
+                province.save();
             }
-            province.save();
+            return true;
         }
-        return true;
+        return false;*/
+
+        if (!TextUtils.isEmpty(jsonData)) {
+            try {
+                JSONArray allProvices = new JSONArray(jsonData);
+                for (int i = 0; i < allProvices.length(); i++) {
+                    JSONObject provinceObject = allProvices.getJSONObject(i);
+                    Province province = new Province();
+                    province.setName(provinceObject.getString("name"));
+                    province.setCode(provinceObject.getInt("id"));
+                    province.save();
+                }
+                return true;
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        return false;
+
     }
+
+    /*Parse and handle city data*/
+    public static boolean handleCityData(String jsonData, int provinceId) {
+        if (!TextUtils.isEmpty(jsonData)) {
+            try {
+                JSONArray allCities = new JSONArray(jsonData);
+                for (int i = 0; i < allCities.length(); i++) {
+                    JSONObject cityObject = allCities.getJSONObject(i);
+                    City city = new City();
+                    city.setName(cityObject.getString("name"));
+                    city.setCode(cityObject.getInt("id"));
+                    city.setProvinceId(provinceId);
+                    city.save();
+                }
+                return true;
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        return false;
+    }
+
+    /*Parse and handle county data*/
+    public static boolean handleCountyData(String jsonData, int cityId) {
+        if (!TextUtils.isEmpty(jsonData)) {
+            try {
+                JSONArray allCounties = new JSONArray(jsonData);
+                for (int i = 0; i<allCounties.length(); i++) {
+                    JSONObject coutyObject = allCounties.getJSONObject(i);
+                    County county = new County();
+                    county.setName(coutyObject.getString("name"));
+                    county.setWeatherId(coutyObject.getString("weather_id"));
+                    county.setCityId(cityId);
+                    county.save();
+                }
+                return true;
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        return false;
+    }
+
 }
